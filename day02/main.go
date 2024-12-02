7c81b392e07fd6daaa1f881b4b9343aaebccdeba
Nate McIntosh <natemcintosh@gmail.com>
2024-12-02 13:11:04 -0500

    day 2 part 2 complete

--- day02/main.go
+++ day02/main.go
@@ -35,16 +35,16 @@ func part1(reports [][]int) int {
 	count := 0
 	// For each report
 	for _, r := range reports {
-		if report_is_good(r) {
+		// Create the diff
+		diffs := diff(r)
+		if report_is_good(diffs) {
 			count += 1
 		}
 	}
 	return count
 }
 
-func report_is_good(report []int) bool {
-	// Create the diff
-	diffs := diff(report)
+func report_is_good(diffs []int) bool {
 
 	// If the diffs aren't entirely positive or enitrely negative, fail
 	if !(all_true(diffs, is_inc) || all_true(diffs, is_dec)) {
@@ -100,6 +100,37 @@ func ge1(diff int) bool { return abs(diff) >= 1 }
 // le3 is true if the difference is less than or equal to 3
 func le3(diff int) bool { return abs(diff) <= 3 }
 
+func report_is_good_p2(report []int, diffs []int) bool {
+	// First check if it is valid via part 1
+	if report_is_good(diffs) {
+		return true
+	}
+
+	// Try removing each element and checking if it is valid
+	for idx := range report {
+		// Create a new report by removing the element at index i
+		new_report := make([]int, len(report)-1)
+		copy(new_report, report[:idx])
+		copy(new_report[idx:], report[idx+1:])
+		new_diffs := diff(new_report)
+		if report_is_good(new_diffs) {
+			return true
+		}
+	}
+	return false
+}
+
+func part2(reports [][]int) int {
+	count := 0
+	for _, r := range reports {
+		diffs := diff(r)
+		if report_is_good_p2(r, diffs) {
+			count += 1
+		}
+	}
+	return count
+}
+
 // The input text of the puzzle
 //
 //go:embed input.txt
@@ -122,13 +153,13 @@ func main() {
 	fmt.Printf("Part 1: %v\n", p1)
 
 	// === Part 2 ====================================================
-	// p2_start := time.Now()
-	// p2 := part2(l, r)
-	// p2_time := time.Since(p2_start)
-	// fmt.Printf("Part 2: %v\n", p2)
+	p2_start := time.Now()
+	p2 := part2(reports)
+	p2_time := time.Since(p2_start)
+	fmt.Printf("Part 2: %v\n", p2)
 
 	// === Print Results ============================================
 	fmt.Printf("\n\nSetup took %v\n", parse_time)
 	fmt.Printf("Part 1 took %v\n", p1_time)
-	// fmt.Printf("Part 2 took %v\n", p2_time)
+	fmt.Printf("Part 2 took %v\n", p2_time)
 }
--- day02/main_test.go
+++ day02/main_test.go
@@ -47,9 +47,47 @@ func TestReport_is_good(t *testing.T) {
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			got := report_is_good(tc.report)
-
+			d := diff(tc.report)
+			got := report_is_good(d)
 			assert.Equal(t, tc.want, got)
 		})
 	}
+}
+
+func TestReport_is_good_p2(t *testing.T) {
+	reports := parse(test_input)
+	want_vals := []bool{true, false, false, true, true, true}
+	tests := []struct {
+		name   string
+		report []int
+		want   bool
+	}{
+		{"good", reports[0], want_vals[0]},
+		{"bad", reports[1], want_vals[1]},
+		{"bad", reports[2], want_vals[2]},
+		{"good", reports[3], want_vals[3]},
+		{"good", reports[4], want_vals[4]},
+		{"good", reports[5], want_vals[5]},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			d := diff(tc.report)
+			got := report_is_good_p2(tc.report, d)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := parse(test_input)
+	got := part2(input)
+	expected := 4
+	assert.Equal(t, expected, got)
+}
+
+func TestPart2Real(t *testing.T) {
+	input := parse(raw_text)
+	got := part2(input)
+	expected := 692
+	assert.Equal(t, expected, got)
 }
