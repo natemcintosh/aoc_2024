8714d3ce9b33ce56cdf9bf19d847dbc13d054ed5
Nate McIntosh <natemcintosh@gmail.com>
2025-02-23 13:15:45 -0500

    some good progress on day 24

--- day24/main.go
+++ day24/main.go
@@ -5,80 +5,230 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"slices"
+	"strings"
 	"time"
 )
 
-// ID is the three letters that make up the name of a node.
-type ID [3]rune
+// Wire is the three letters that make up the name of a node.
+type Wire [3]rune
 
-func (id ID) String() string {
-	return fmt.Sprintf("%c%c%c", id[0], id[1], id[2])
-}
-
-// node_info stores information about a given node
-type node_info struct {
-	// id is the name of the given node
-	id ID
-
-	// npredecessors
-	npredecessors int
-
-	// successors is the slice of successor nodes. Can hold duplicates, as long as
-	// they're all reflected in the successor's npredecessors count
-	successors []ID
-}
-
-func new_node_info(node ID) node_info {
-	return node_info{node, 0, make([]ID, 0)}
-}
-
-type TopoSorter struct {
-	// node2info contains the map of IDs to info
-	node2info    map[ID]node_info
-	ready_nodes  []ID
-	n_passed_out int
-	n_finished   int
-}
-
-// get_node_info finds the information for an existing node. If the node is not already
-// in the map, then it will add it.
-// In the python code, this function is used to both add non-existent items to
-// ts.node2info, AND alter the values in ts.node2info
-func (ts *TopoSorter) get_node_info(node ID) *node_info {
-	result, exists := ts.node2info[node]
-	if !exists {
-		result = new_node_info(node)
-		ts.node2info[node] = result
-	}
-	return &result
-}
-
-// Add will add this node to the TopoSorter.
-//
-// If called multiple times with the same node argument, the set of dependencies will
-// be the union of all dependencies pass in.
-//
-// It is possible to add a node with no dependencies, as well as provide a dependency
-// twice. If a node that has not been provided before is included among `predecessors`
-// it will be automatically added to the graph with no predecessors of its own.
-//
-// Throws an error if called after "prepare"
-func (ts *TopoSorter) Add(node ID, predecessors ...ID) error {
-	if len(ts.ready_nodes) == 0 {
-		return fmt.Errorf("Nodes cannot be added after a call to prepare.")
+// NewWire creates a Wire from a string exactly 3 letters long. All letters must be valid
+// ASCII
+func NewWire(s string) Wire {
+	if len(s) != 3 {
+		panic("ID must be exactly 3 characters long")
 	}
 
-	// Create the node -> predecessor edges
-	node_info := ts.get_node_info(node)
-	node_info.npredecessors += len(predecessors)
-
-	// Create the predecessor -> node edges
-	for _, pred := range predecessors {
-		pred_info := ts.get_node_info(pred)
-		pred_info.successors = append(pred_info.successors, node)
+	var wire Wire
+	for idx, r := range s {
+		if r > 127 {
+			panic("ID must be valid ASCII")
+		}
+		wire[idx] = r
 	}
 
-	return nil
+	return wire
+}
+
+// String shows the three letters in an easy to read manner
+func (wire Wire) String() string {
+	return fmt.Sprintf("%c%c%c", wire[0], wire[1], wire[2])
+}
+
+func (wire Wire) Format(f fmt.State, c rune) {
+	switch c {
+	case 's':
+		fmt.Fprintf(f, "%s", wire.String())
+	case 'v':
+		fmt.Fprintf(f, "%v", wire.String())
+	default:
+		fmt.Fprintf(f, "%s", wire.String())
+	}
+}
+
+// NameFromInt fetches the original Wire that created this int64. Assumes that all wires
+// are created only from valid ASCII characters.
+func NameFromInt(n int64) Wire {
+	return Wire{
+		rune((n >> 32) & 0xFF),
+		rune((n >> 16) & 0xFF),
+		rune(n & 0xFF),
+	}
+}
+
+// ID merges the three runes into a single int64. Because the runes are all valid ASCII,
+// and we keep them shifted 16 places apart, they won't overlap and mess eachother up.
+// It should in theory not produce hash collisions for this case.
+func (wire Wire) ID() int64 {
+	return (int64(wire[0]) << 32) | (int64(wire[1]) << 16) | int64(wire[2])
+}
+
+// DiGraph is a directed graph that stores the edges in both directions. This is
+// necessary to be able to traverse the graph in both directions. The graph is
+// represented as a map of edges.
+type DiGraph struct {
+	from_to map[Wire][]Wire
+	to_from map[Wire][]Wire
+}
+
+func NewDiGraph() DiGraph {
+	return DiGraph{
+		from_to: make(map[Wire][]Wire),
+		to_from: make(map[Wire][]Wire),
+	}
+}
+
+// AddEdge adds an edge to the graph. It adds the edge in both directions.
+func (g *DiGraph) AddEdge(from, to Wire) {
+	g.from_to[from] = append(g.from_to[from], to)
+	g.to_from[to] = append(g.to_from[to], from)
+
+	// Make sure that there is an entry (even if it is empty) for `to` in the from_to
+	// map. This is necessary to be able to traverse the graph in both directions.
+	if _, ok := g.from_to[to]; !ok {
+		g.from_to[to] = []Wire{}
+	}
+
+	// Make sure that there is an entry (even if it is empty) for `from` in the to_from
+	// map. This is necessary to be able to traverse the graph in both directions.
+	if _, ok := g.to_from[from]; !ok {
+		g.to_from[from] = []Wire{}
+	}
+}
+
+// RemoveEdge removes an edge from the graph. It removes the edge in both directions.
+func (g *DiGraph) RemoveEdge(from, to Wire) {
+	g.from_to[from] = slices.DeleteFunc(g.from_to[from], func(w Wire) bool { return w == to })
+	g.to_from[to] = slices.DeleteFunc(g.to_from[to], func(w Wire) bool { return w == from })
+}
+
+// HasEdges checks if there are any edges in the DiGraph at all.
+func (g *DiGraph) HasEdges() bool {
+	// Check that all from_to slices are not empty
+	for _, v := range g.from_to {
+		if len(v) > 0 {
+			return true
+		}
+	}
+
+	// Check that all to_from slices are not empty
+	for _, v := range g.to_from {
+		if len(v) > 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+// TopoSort performs a topological sort on the graph. It returns a slice of wires in
+// topological order. It returns an error if the graph is not a DAG (Directed Acyclic
+// Graph). This is performed using [Kahn's Algorithm](wikipedia.org/Topological_sorting#Kahn's_algorithm)
+func (g *DiGraph) TopoSort() ([]Wire, error) {
+	// Create an empty slices that wil container all the sorted elements
+	L := make([]Wire, 0, len(g.from_to))
+
+	// Create a slices that we will iterate over. Start with all nodes that have no
+	// incoming edges
+	S := make([]Wire, 0)
+
+	// Populate S with all nodes that have no incoming edges
+	// This is done by checking the to_from map. If a node has no incoming edges, it will
+	// have an empty slice in the to_from map.
+	for k, v := range g.to_from {
+		if len(v) == 0 {
+			S = append(S, k)
+		}
+	}
+
+	// If nothing in S, then fail
+	if len(S) == 0 {
+		return nil, fmt.Errorf("no nodes with no incoming edges")
+	}
+
+	// While S is not empty, remove a node from S and add it to L
+	for len(S) > 0 {
+		// Remove a node n from S, and add it to L
+		n := S[len(S)-1]
+		S = S[:len(S)-1]
+
+		// Add node n to L
+		L = append(L, n)
+
+		// For each node m with an edge e from n to m, remove the edge e from the graph
+		for idx := 0; idx < len(g.from_to[n]); idx++ {
+			m := g.from_to[n][idx]
+			g.RemoveEdge(n, m)
+			// Decrement the index to account for the removed edge
+			idx--
+
+			// If m has not other incoming edges, add it to S
+			if len(g.to_from[m]) == 0 {
+				S = append(S, m)
+			}
+		}
+	}
+
+	// If the graph still has edges, there there is at least one cycle in the graph
+	if g.HasEdges() {
+		return nil, fmt.Errorf("graph has edges")
+	}
+
+	return L, nil
+}
+
+type Op int
+
+const (
+	AND Op = iota
+	OR
+	XOR
+)
+
+func NewOp(s string) Op {
+	switch s {
+	case "AND":
+		return AND
+	case "OR":
+		return OR
+	case "XOR":
+		return XOR
+	default:
+		panic("Invalid operation")
+	}
+}
+
+type Gate struct {
+	// The input wires
+	in1, in2 Wire
+
+	// The output wire
+	out Wire
+
+	// The operation to perform
+	op Op
+}
+
+// NewGate creates a new gate from a string. Below are examples of the strings that can
+// be passed in:
+// ntg XOR fgs -> mjb
+// y02 OR x01 -> tnw
+// kwq OR kpj -> z05
+// x00 OR x03 -> fst
+// tgd XOR rvg -> z01
+// vdt OR tnw -> bfw
+// bfw AND frj -> z10
+// ffh OR nrd -> bqk
+func NewGate(s string) Gate {
+	// Split around spaces
+	parts := strings.Split(s, " ")
+	return Gate{
+		NewWire(parts[0]),
+		NewWire(parts[2]),
+		NewWire(parts[4]),
+		NewOp(parts[1]),
+	}
 }
 
 // The input text of the puzzle
--- day24/main_test.go
+++ day24/main_test.go
@@ -1,1 +1,132 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// FuzzNameFromID is a fuzz test that validates encoding and decoding of an ID.
+// func FuzzNameFromID(f *testing.F) {
+// 	// Seed with some valid 3-character ASCII strings.
+// 	for r1 := rune(32); r1 < 127; r1++ {
+// 		f.Add(r1, r1+1, r1+2)
+// 	}
+
+// 	f.Fuzz(func(t *testing.T, r1, r2, r3 rune) {
+// 		// Check if the runes are valid ASCII. Also skip early ASCII characters before
+// 		// space, as they are not printable.
+// 		if r1 > 127 || r2 > 127 || r3 > 127 || r1 < 32 || r2 < 32 || r3 < 32 {
+// 			t.Skip("non-ASCII character encountered")
+// 		}
+// 		id := Wire{r1, r2, r3}
+// 		// Encode then decode the ID.
+// 		recovered := NameFromInt(id.ID())
+
+// 		// Check if the original and recovered IDs match.
+// 		if id != recovered {
+// 			t.Errorf("expected %+v, got %+v", id, recovered)
+// 		}
+// 	})
+// }
+
+func BenchmarkID(b *testing.B) {
+	// Create an ID, then encode and decode it.
+	id := Wire{'a', 'b', 'c'}
+	for b.Loop() {
+		NameFromInt(id.ID())
+	}
+}
+
+func TestNewID(t *testing.T) {
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{"abc", false},
+		{"def", false},
+		{"ghi", false},
+		{"jklm", true},
+		{"", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.wantErr {
+				assert.Panics(t, func() { NewWire(tt.name) })
+				return
+			}
+		})
+	}
+}
+
+// TestConstructDiGraph tests the creation of the map
+// ┌──A──┐
+// ▼     ▼
+// B     C
+// │     │
+// │     ▼
+// └────►D
+func TestConstructDiGraph(t *testing.T) {
+	// Create a new directed g
+	g := NewDiGraph()
+
+	// Add the edges
+	g.AddEdge(NewWire("AAA"), NewWire("BBB"))
+	g.AddEdge(NewWire("AAA"), NewWire("CCC"))
+	g.AddEdge(NewWire("BBB"), NewWire("DDD"))
+	g.AddEdge(NewWire("CCC"), NewWire("DDD"))
+
+	// Check that the internal maps are as expected
+	// First the from-to slices, in alphabetical order: A, B, C, D
+	// From A
+	assert.Equal(t, []Wire{NewWire("BBB"), NewWire("CCC")}, g.from_to[NewWire("AAA")])
+
+	// From B
+	assert.Equal(t, []Wire{NewWire("DDD")}, g.from_to[NewWire("BBB")])
+
+	// From C
+	assert.Equal(t, []Wire{NewWire("DDD")}, g.from_to[NewWire("CCC")])
+
+	// From D
+	assert.Equal(t, []Wire{}, g.from_to[NewWire("DDD")])
+
+	// To A
+	assert.Equal(t, []Wire{}, g.to_from[NewWire("AAA")])
+
+	// To B
+	assert.Equal(t, []Wire{NewWire("AAA")}, g.to_from[NewWire("BBB")])
+
+	// To C
+	assert.Equal(t, []Wire{NewWire("AAA")}, g.to_from[NewWire("CCC")])
+
+	// To D
+	assert.Equal(t, []Wire{NewWire("BBB"), NewWire("CCC")}, g.to_from[NewWire("DDD")])
+}
+
+// TestTopoSort tests that we get the proper ordering for this map
+// ┌──A──┐
+// ▼     ▼
+// B     C
+// │     │
+// │     ▼
+// └────►D
+func TestTopoSort(t *testing.T) {
+	a := NewWire("AAA")
+	b := NewWire("BBB")
+	c := NewWire("CCC")
+	d := NewWire("DDD")
+
+	// Create and populate the graph
+	g := NewDiGraph()
+	g.AddEdge(a, b)
+	g.AddEdge(a, c)
+	g.AddEdge(b, d)
+	g.AddEdge(c, d)
+
+	want := []Wire{a, c, b, d}
+	got, err := g.TopoSort()
+	if err != nil {
+		t.Fatal("Found a cycle when should not have")
+	}
+	assert.Equal(t, want, got)
+}
